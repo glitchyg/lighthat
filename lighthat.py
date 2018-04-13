054a1b90cd5d90f1180d60090d67d2bb34bf7a4e
JMO <git@jason-morano.com>
2018-04-13 11:14:58 -0400

    Fast Commit

--- lighthat.py
+++ lighthat.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
 
         while True:
             # hatDisplay.colorWipe(strip, Color(0, 0, 0), 0)
-            hatDisplay.show_simple_text(strip, "HELLO")
+            hatDisplay.show_simple_text(strip, "HELLO WORLD")
             time.sleep(60)
             # print('Color wipe animations.')
             # colorWipe(strip, Color(255, 0, 0), 10)  # Red wipe
