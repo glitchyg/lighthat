5fc880b305d008c7e3f1e13420bc2e7a0dd2898e
JMO <git@jason-morano.com>
2018-04-13 22:45:08 -0400

    Fast Commit

--- settingscontroller.py
+++ settingscontroller.py
@@ -45,6 +45,7 @@ current_mode = 0
 
 
 def merge_two_dicts(x, y):
+    print(x["text_text_color"])
     z = x.copy()  # start with x's keys and values
     z.update(y)  # modifies z with y's keys and values & returns None
     return z
