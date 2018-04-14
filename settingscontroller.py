91279bf341ffbff4cd5554fdbe9d6cccf68df77b
JMO <git@jason-morano.com>
2018-04-14 00:34:53 -0400

    Fast Commit

--- settingscontroller.py
+++ settingscontroller.py
@@ -53,11 +53,11 @@ modes = {
     },
     "text_and_chase": {
         "mode": MODE_TEXT_AND_CHASE,
-        "text": "#SWAG",
+        "text": "LIGHT CITY",
         "text_bg_color": (0, 0, 0),
         "text_text_color": (255, 255, 255),
         "text_start_offset": 23,
-        "text_scroll_speed": 0,
+        "text_scroll_speed": -0.2,
         "chase_color": Color(255, 0, 0),  # G, R, B
         "chase_delay": 20,
         "text_show_strip": True,
