f9ac032d5867409e27f074ff42d4adfd0b271f77
JMO <git@jason-morano.com>
2018-04-19 12:55:09 -0400

    Fast Commit

--- webinterface.py
+++ webinterface.py
@@ -2,11 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+strip = None
 
 @app.route("/")
 def hello():
+    strip.setBrightness(60)
     return "Hello World!"
 
 
-def web_interface_thread(settings):
+def web_interface_thread(settings, input_strip):
+    global strip
+    strip = input_strip
     app.run()
